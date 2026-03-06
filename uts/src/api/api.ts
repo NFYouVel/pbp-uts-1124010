2564b20876bda6db55f45c097f4a92250fdfdf19
Marvel Nathanael <marvelnathanael072006@gmail.com>
2026-03-06 11:53:56 +0700

    Update api.ts

--- uts/src/api/api.ts
+++ uts/src/api/api.ts
@@ -1,52 +1,5 @@
 const BASE_URL = '/api'
 
-export async function loginRequest(email: string, password: string) {
-    const response = await fetch(`${BASE_URL}/auth/login`, {
-        method: "POST",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify({
-            email,
-            password
-        })
-    });
-
-    const data = await response.json();
-
-    return {
-        status: response.status,
-        data
-    };
-}
-
-export async function getUserInfo() {
-    const response = await fetch(`${BASE_URL}/auth/me`, {
-        method: "GET",
-        headers: {
-            "content-type": "application/json"
-        }
-    });
-
-    const data = await response.json();
-
-    return data;
-}
-
-
-export async function getPostRequest () {
-    const response = await fetch(`${BASE_URL}/post`, {
-        method: "GET",
-        headers: {
-            "content-type": "application/json"
-        }
-    })
-    const data = await response.json();
-    console.log(data);
-
-    return data;
-}
-
 export async function getBooksRequest() {
     const response = await fetch(`${BASE_URL}/buku`, {
         method: "GET",
