3a91fdbeeaaffbc035286eaa296a6ae7f91881c3
Marvel Nathanael <marvelnathanael072006@gmail.com>
2026-03-06 11:51:42 +0700

    updated

--- uts/src/pages/HomePage.tsx
+++ uts/src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import { useAppDispatch } from "../hooks/useAppDispatch";
 import { useEffect } from "react";
 import { getBooksRequest } from "../api/api";
 import { bookAction } from "../store/bookSlice";
-import { Card, CardContent, Typography, CardActions, Button, CardMedia } from "@mui/material";
+import { Card, CardContent, Typography, CardActions, Button, CardMedia, Grid, Container } from "@mui/material";
 import { Link } from "react-router";
 import { useSelector } from "react-redux";
 import type { RootState } from "../store/store";
@@ -28,36 +28,39 @@ function HomePage() {
     }, [dispatch])
 
     return (
-        <div className="post-container" style={{display: "flex", gap: "20px", width: "10000px", position:"relative"
-        }}>
-            {books?.map((book) => (
-                <Card sx={{ width: "75%", height:"auto", marginBottom: 2, bgcolor: 'grey.200', marginTop:"100px" }} key={book.id}>
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {book.judul}
-                        </Typography>
-                        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                            {book.deskripsi}
-                        </Typography>
-                    </CardContent>
-                    <CardMedia
-                        component="img"
-                        height="200px"
-                        width="100px"
-                        image={book.imageUrl}
-                    />
-                    <CardActions>
-                        <Link to={'/book-details/' + book.id}>
-                            <Button size="small">View Details</Button>
-                        </Link>
-                        
-                        <Link to={'/book-update/' + book.id}>
-                            <Button size="small">Update Book</Button>
-                        </Link>
-                    </CardActions>
-                </Card>
-            ))}
-        </div>
+        <Container>
+            <Grid container>
+                {books?.map((book) => (
+                    <Grid size={3}>
+                        <Card sx={{ height: "auto", marginBottom: 2, marginRight: 2, bgcolor: 'grey.200' }} key={book.id}>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {book.judul}
+                                </Typography>
+                                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                    {book.deskripsi}
+                                </Typography>
+                            </CardContent>
+                            <CardMedia
+                                component="img"
+                                height="200px"
+                                width="100px"
+                                image={book.imageUrl}
+                            />
+                            <CardActions>
+                                <Link to={'/book-details/' + book.id}>
+                                    <Button size="small">View Details</Button>
+                                </Link>
+
+                                <Link to={'/book-update/' + book.id}>
+                                    <Button size="small">Update Book</Button>
+                                </Link>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
     )
 }
 
